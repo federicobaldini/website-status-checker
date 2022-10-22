ca747ae9f1699b650f5a58f4fd16b430935c0ed3
Federico Baldini <federico.baldini7@gmail.com>
2022-10-22 12:22:46 +0200

    Update main.go
    Added checkLink function.

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
 func main() {
 
+func checkLink(link string, channel chan string) {
+	start := time.Now()
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down! response time:", time.Since(start))
+		channel <- link
+		return
+	}
+	fmt.Println(link, "is up! response time:", time.Since(start))
+	channel <- link
 }
