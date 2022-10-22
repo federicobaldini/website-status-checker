545eecb6d00f52763918f2f9067765d962d5b0fa
Federico Baldini <federico.baldini7@gmail.com>
2022-10-22 12:23:02 +0200

    Update main.go
    Used checkLink function.

--- main.go
+++ main.go
@@ -7,6 +7,26 @@ import (
 )
 
 func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://goland.org",
+		"http://amazon.com",
+	}
+	channel := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, channel)
+	}
+
+	for l := range channel {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, channel)
+		}(l)
+	}
+}
 
 func checkLink(link string, channel chan string) {
 	start := time.Now()
